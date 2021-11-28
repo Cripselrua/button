64ba7553b58c7ea8fbfcf5e84fe3da19a4fb4be9
Cripselrua <90082950+Cripselrua@users.noreply.github.com>
2021-11-28 19:06:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict"
+$(function(){
+  $("#tab_1") .load ("vip/vip.html"); 
+});
+ 
+ $(function(){
+    $("#tab_2") .load ("apartment/apartment_sell.html"); 
+  });
+  
+  $(function(){
+    $("#tab_3") .load ("apartment/apartment_buy.html"); 
+  });
+ 
+  $(function(){
+    $("#tab_4") .load ("apartment/apartment_rent.html"); 
+  });
+
+
+  
+
+  
+     	
+
+
+// ================ домов ========================
