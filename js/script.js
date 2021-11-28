b19aec9360d1e03560d715a96dc921db311b5ba9
Cripselrua <90082950+Cripselrua@users.noreply.github.com>
2021-11-28 16:54:20 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(function(){
-  $("#tab_1") .load ("../vip/vip.html"); 
+  $("#tab_1") .load ("./vip/vip.html"); 
 });
  
  $(function(){
-    $("#tab_2") .load ("../apartment/apartment_sell.html"); 
+    $("#tab_2") .load ("./apartment/apartment_sell.html"); 
   });
   
   $(function(){
-    $("#tab_3") .load ("../apartment/apartment_buy.html"); 
+    $("#tab_3") .load ("./apartment/apartment_buy.html"); 
   });
  
   $(function(){
-    $("#tab_4") .load ("../apartment/apartment_rent.html"); 
+    $("#tab_4") .load ("./apartment/apartment_rent.html"); 
   });
 
 
